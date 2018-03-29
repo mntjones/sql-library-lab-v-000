04adfdc1015cdb27f0e04b4a105ca2f31a220485
Monica Tamaru Jones <mntamaru@gmail.com>
2018-03-29 20:48:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ def select_series_title_with_most_human_characters
   
   # grab the series title, which has books, which has characters, which have species.
   # I want to look at all the characters in a series and group by species
-  # then I want to count the number of characters in the species groups
+  # then I want to count the number of characters in the species groups and return the one 
 end
 
 def select_character_names_and_number_of_books_they_are_in
