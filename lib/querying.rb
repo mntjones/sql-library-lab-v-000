4f1d63c441e245833c49e4489ac72ea625a52e5b
Monica Tamaru Jones <mntamaru@gmail.com>
2018-03-29 22:22:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,5 +22,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT character.name, COUNT(books) FROM characters INNER JOIN character_books WHERE ;"
   
-  # 
+  # Characters have a name, motto, and species and belong to an author and a series
+  
 end
